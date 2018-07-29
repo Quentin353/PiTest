98b8443192ec4b1cfe421e2d630b50a72d351d22
unknown <quentin353@hotmail.fr>
2018-07-29 19:06:33 +0200

    Signed-off-by: unknown <quentin353@hotmail.fr>

--- LED.py
+++ LED.py
@@ -4,9 +4,9 @@ import time
 
 GPIO.setwarnings(False)
 
-GPIO_LED_GREEN  = 18
-GPIO_LED_RED    = 17
-GPIO_LED_BLUE   = 23
+GPIO_LED_GREEN  = 17
+GPIO_LED_RED    = 4
+GPIO_LED_BLUE   = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_LED_BLUE, GPIO.OUT,initial = GPIO.HIGH)
@@ -14,7 +14,7 @@ GPIO.setup(GPIO_LED_RED, GPIO.OUT,initial = GPIO.HIGH)
 GPIO.setup(GPIO_LED_GREEN, GPIO.OUT,initial = GPIO.HIGH)
 
 # loop through 10 times, on/off for 1 second
-for i in range(10):
+for i in range(5):
     GPIO.output(GPIO_LED_BLUE,True)
     time.sleep(1)
     GPIO.output(GPIO_LED_RED,True)
