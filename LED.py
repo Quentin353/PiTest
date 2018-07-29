772470eb3f8473d32f41535b2fedeaa7ad095d65
unknown <quentin353@hotmail.fr>
2018-07-29 17:32:52 +0200

    Push depuis l'ordi

--- LED.py
+++ LED.py
@@ -1,12 +1,33 @@
 #import the GPIO and time package
 import RPi.GPIO as GPIO
 import time
+
+GPIO_LED_GREEN  = 35
+GPIO_LED_RED    = 37
+GPIO_LED_BLUE   = 11
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT,initial = GPIO.HIGH)
-# loop through 50 times, on/off for 1 second
-for i in range(50):
-    GPIO.output(11,True)
+GPIO.setup(GPIO_LED_BLUE, GPIO.OUT,initial = GPIO.HIGH)
+GPIO.setup(GPIO_LED_RED, GPIO.OUT,initial = GPIO.HIGH)
+
+# loop through 10 times, on/off for 1 second
+for i in range(10):
+    GPIO.output(GPIO_LED_BLUE,True)
     time.sleep(1)
-    GPIO.output(11,False)
+    GPIO.output(GPIO_LED_RED,True)
+    time.sleep(1)
+    GPIO.output(GPIO_LED_GREEN,True)
+    time.sleep(1)
+    GPIO.output(GPIO_LED_BLUE,False)
+   	GPIO.output(GPIO_LED_RED,False)
+  	GPIO.output(GPIO_LED_GREEN,False)
+    time.sleep(1)
+    GPIO.output(GPIO_LED_BLUE,True)
+   	GPIO.output(GPIO_LED_RED,True)
+  	GPIO.output(GPIO_LED_GREEN,True)
+    time.sleep(1)
+    GPIO.output(GPIO_LED_BLUE,False)
+   	GPIO.output(GPIO_LED_RED,False)
+  	GPIO.output(GPIO_LED_GREEN,False)
     time.sleep(1)
 GPIO.cleanup()
