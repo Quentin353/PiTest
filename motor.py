99c3de13b7c1b8d934753f381b5977efa4f5368a
unknown <quentin353@hotmail.fr>
2018-07-30 15:25:17 +0200

    Test motor
    Signed-off-by: unknown <quentin353@hotmail.fr>

--- motor.py
+++ motor.py
@@ -6,12 +6,15 @@ GPIO.setmode(GPIO.BOARD)
 Motor1A = 16
 Motor1B = 18
 Motor1E = 22
+LED_VErt = 7
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
+GPIO.setup(LED_VErt,GPIO.OUT)
  
 print "Turning motor on"
+GPIO.output(LED_VErt,GPIO.HIGH)
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
@@ -20,5 +23,7 @@ sleep(2)
  
 print "Stopping motor"
 GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(LED_VErt,GPIO.LOW)
+
  
 GPIO.cleanup()
