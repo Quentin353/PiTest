9da24589161e594af985d8fb1f81184ae228830d
Quentin <quentin353@hotmail.fr>
2018-07-31 17:35:17 +0200

    Signed-off-by: unknown <quentin353@hotmail.fr>

--- motor.py
+++ motor.py
@@ -5,29 +5,76 @@ from time import sleep
  
 GPIO.setmode(GPIO.BOARD)
  
+#moteur 1
 Motor1A = 16
 Motor1B = 18
 Motor1E = 22
-LED_VErt = 7
 
+#moteur 2
+Motor2A = 11
+Motor2B = 40
+Motor2E = 15
+
+LED_VErt = 7
+GPIO.setup(LED_VErt,GPIO.OUT)
+
+
+GPIO.output(LED_VErt,GPIO.HIGH)
+sleep(1)
+GPIO.output(LED_VErt,GPIO.LOW)
 sleep(2)
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
 GPIO.setup(LED_VErt,GPIO.OUT)
+
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
  
 print "Turning motor on"
 GPIO.output(LED_VErt,GPIO.HIGH)
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
+
+GPIO.output(Motor2A,GPIO.HIGH)
+GPIO.output(Motor2B,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.HIGH)
  
 sleep(2)
  
 print "Stopping motor"
 GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.LOW)
+
 GPIO.output(LED_VErt,GPIO.LOW)
+
+sleep(2)
+
+print "Turning motor on, dans l'autre sens"
+GPIO.output(LED_VErt,GPIO.HIGH)
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
+GPIO.output(Motor1E,GPIO.HIGH)
+
+GPIO.output(Motor2A,GPIO.LOW)
+GPIO.output(Motor2B,GPIO.HIGH)
+GPIO.output(Motor2E,GPIO.HIGH)
+
+sleep(2) 
+
+print "Stopping motor"
+GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.LOW)
+GPIO.output(LED_VErt,GPIO.LOW)
+
+sleep(2)
+GPIO.output(LED_VErt,GPIO.HIGH)
+sleep(1)
+GPIO.output(LED_VErt,GPIO.LOW)
+sleep(2)
 
  
 GPIO.cleanup()
